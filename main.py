e0d0fac0c1d5c6f9f2f91a031372a5ed37f394c6
rachelkelly <rachelkelly@gmail.com>
2013-06-22 13:51:38 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+#from www.pythonware.com/library/tkinter/introduction/hello-tkinter.htm
+#it's all given in 2.7 so some of my conversions might be flawnky
+#hella, hella adapted & slammed into place from my random hw generator.
+#make it work, shoooooooot.
+
+from tkinter import *
+
+class App:
+
+  def __init__(self, master):
+
+		frame = Frame(master)
+		frame.pack()
+
+		self.button = Button(frame, text="QUIT", fg="red", bg="yellow", command=frame.quit)
+		self.button.pack(side=LEFT)
+
+		self.randotron = Button(frame, text="randomize?", fg="blue", bg="gray", command=self.randotron)
+		self.randotron.pack(side=LEFT)
+
+	def randotron(self):
+## this is where the cool stuff goes!!
+## I'm gonna put the random module here instead
+		import random
+		problems = []
+		i = 0
+
+		section = input("what section is today's homework? ")
+		amount = int(input("\nhow many problems today? "))
+		while i < amount:
+			problem = int(input("what is the problem number? "))
+			problems.append(problem)
+			i += 1
+
+		print("From section",section,"there are",amount,"problems, which are the following\n")
+		print(problems)
+
+		problemChoice = random.choice(problems)
+		#whoa, random.choice might work!  thanks steve from pyladies!
+		print("The problem to grade is",problemChoice)
+		print("(_|_)")
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
